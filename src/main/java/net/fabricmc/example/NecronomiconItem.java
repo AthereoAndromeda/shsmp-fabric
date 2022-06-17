d7c555d48e406bc1735c06c6a97b9d6c5b89b25f
AthereoAndromeda <athereoandromeda@gmail.com>
2022-06-18 01:58:01 +0800

    Add sound to Necronomicon

--- src/main/java/net/fabricmc/example/NecronomiconItem.java
+++ src/main/java/net/fabricmc/example/NecronomiconItem.java
@@ -1,6 +1,13 @@
 package net.fabricmc.example;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 public class NecronomiconItem extends Item {
 
@@ -8,4 +15,9 @@ public class NecronomiconItem extends Item {
         super(settings);
     }
     
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        player.playSound(SoundEvents.AMBIENT_CAVE, 1.0F, 1.0F);
+        return new TypedActionResult<ItemStack>(ActionResult.SUCCESS, player.getStackInHand(hand));
+    }
 }
