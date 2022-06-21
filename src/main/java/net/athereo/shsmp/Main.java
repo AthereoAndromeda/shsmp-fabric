8070c0ff408f0aac5a92a05516689f53cf469686
AthereoAndromeda <athereoandromeda@gmail.com>
2022-06-21 19:32:26 +0800

    (WIP) Add teams

--- src/main/java/net/athereo/shsmp/Main.java
+++ src/main/java/net/athereo/shsmp/Main.java
@@ -6,6 +6,8 @@ import net.athereo.shsmp.item.NecronomiconItem;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -19,6 +21,10 @@ public class Main implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final Item item = new NecronomiconItem(new Item.Settings().group(ItemGroup.MISC));
 
+	public static final Scoreboard scoreboard = new Scoreboard();
+	public static Team aliveTeam = scoreboard.getTeam("alive");
+	public static Team deadTeam = scoreboard.getTeam("dead");
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -28,12 +34,27 @@ public class Main implements ModInitializer {
 		LOGGER.info("Initializing SHSMP-Fabric...");
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "necronomicon"), item);
 
+		if (aliveTeam == null) {
+			aliveTeam = scoreboard.addTeam("alive");
+			LOGGER.info("Alive team created");
+		}
+
+		if (deadTeam == null) {
+			deadTeam = scoreboard.addTeam("dead");
+			LOGGER.info("Dead team created");
+		}
+
 		PlayerDeathCallback.EVENT.register((player, source) -> {
 			LOGGER.info(player.getEntityName());
 			LOGGER.info(source.getName());
 
 			player.sendMessage(Text.literal("§4§lSigh. Better luck next time."));
 			player.changeGameMode(GameMode.SPECTATOR);
+			scoreboard.addPlayerToTeam(player.getEntityName(), deadTeam);
+
+			for (String str : deadTeam.getPlayerList()) {
+				LOGGER.info(str);
+			}
 		});
 
 		PlayerJoinCallback.EVENT.register((player, server) -> {
