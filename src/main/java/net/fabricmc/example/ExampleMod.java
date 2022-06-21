3efa799780f805bcc90ee1d685d1f576c2bb7ecb
AthereoAndromeda <athereoandromeda@gmail.com>
2022-06-21 19:33:33 +0800

    Refactor comments

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -5,9 +5,7 @@ import net.fabricmc.example.event.PlayerDeathCallback;
 import net.fabricmc.example.item.NecronomiconItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.GameMode;
@@ -16,9 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "shsmp";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final Item item = new NecronomiconItem(new Item.Settings().group(ItemGroup.MISC));
